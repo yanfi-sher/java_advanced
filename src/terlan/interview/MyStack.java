6fa4f2e5811088a59f1ab9f19a9499124ebfe136
yanfi-sher <loss.soul20@gmail.com>
2023-11-15 18:32:32 +0200

    HW 15/11/23 My Stack done

--- src/terlan/interview/MyStack.java
+++ src/terlan/interview/MyStack.java
@@ -1,28 +1,50 @@
 package terlan.interview;
 
+import java.util.LinkedList;
 import java.util.NoSuchElementException;
 
 public class MyStack {
+    private final LinkedList<Integer> stackMap = new LinkedList<>();
+    private int size = 0;
 
-    void push(int number){
-        //добавить номер в конец стека
+    public void push(int number){
+        stackMap.addLast(number);
+        size++;
     }
 
-    int pull(){
+    public int pull(){
+        emptyException();
+        int res = stackMap.getLast();
+        stackMap.remove(size-1);
+        size--;
+        return res;
+
         //возвращается и удаляется последний элемент
         //если пустой стэк, то бросается NoSuchElementException
-        return -1;
     }
 
-    boolean isEmpty(){
+    private void emptyException() {
+        if (isEmpty()) {
+            throw new NoSuchElementException("Empty stack");
+        }
+    }
+
+    public boolean isEmpty(){
         // выясняет пустой стэк или нет
-        return false;
+        return stackMap.isEmpty();
     }
 
-    int getMax(){
+    public int getMax(){
+        emptyException();
+        int max = Integer.MIN_VALUE;
+        for (int x : stackMap) {
+            max = Math.max(max, x);
+        }
+        return max;
         //возвращает наибольшее число, если стек не пустой
         //если пустой стэк, то бросается NoSuchElementException
-        return -1;
+
+        //я думаю, что сложность не та!!!! надо переделать...
     }
 
 }
--- src/terlan/interview/test/MyStackTests.java
+++ src/terlan/interview/test/MyStackTests.java
@@ -1,4 +1,90 @@
 package terlan.interview.test;
 
-public class MyStackTests {
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.NoSuchElementException;
+import java.util.Random;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import terlan.interview.MyStack;
+
+class MyStackTest {
+    private static final long N_NUMBERS = 10000;
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 100;
+    MyStack stack = new MyStack();
+    LinkedList<Integer> list = new LinkedList<>();
+    MyStack stackEmpty = new MyStack();
+    Random gen = new Random();
+
+    @BeforeEach
+    void setUp(){
+        gen.ints(N_NUMBERS, MIN_NUMBER, MAX_NUMBER).
+                forEach(n -> {
+                    stack.push(n);
+                    list.add(n);
+                });
+    }
+
+    @Test
+    void testPull() {
+        assertEquals(list.removeLast(), stack.pull());
+
+        assertThrows(NoSuchElementException.class, () -> stackEmpty.pull());
+    }
+
+    @Test
+    void testPush() {
+        stack.push(-10);
+        assertNotEquals(list.removeLast(), stack.pull());
+    }
+
+    @Test
+    void testIsEmpty() {
+        assertFalse(stack.isEmpty());
+        assertTrue(stackEmpty.isEmpty());
+    }
+
+    @Test
+    void testGetMaxNumber() {
+        testRandom();
+        predefinedGetMaxTest();
+    }
+
+    void testRandom() {
+        for (int i = 0; i < N_NUMBERS; i++) {
+            if (Math.random() * 100 < 50) {
+                try {
+                    stack.pull();
+                    list.removeLast();
+                } catch (Exception ignored) {
+
+                }
+
+            } else {
+                int number = gen.nextInt(MIN_NUMBER, MAX_NUMBER);
+
+                stack.push(number);
+                list.add(number);
+            }
+        }
+        assertEquals((int) Collections.max(list), stack.getMax());
+    }
+
+    void predefinedGetMaxTest() {
+        int[] ar = {100000, 50000, 100000, 20, 20, 20, 2000000};
+        MyStack stack = new MyStack();
+        for (int j : ar) {
+            stack.push(j);
+        }
+        assertEquals(2000000, stack.getMax());
+        stack.pull();
+        stack.pull();
+        stack.pull();
+        assertEquals(100000, stack.getMax());
+    }
 }
