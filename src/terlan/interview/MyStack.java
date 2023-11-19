cae8940fe063080f2719f368d36f12b1038ed0c4
yanfi-sher <loss.soul20@gmail.com>
2023-11-19 09:29:24 +0200

    HW 15/11/23 MyStack, исправил недочеты

--- src/terlan/interview/MyStack.java
+++ src/terlan/interview/MyStack.java
@@ -6,12 +6,10 @@ import java.util.NoSuchElementException;
 public class MyStack {
     private LinkedList<Integer> stackMap = new LinkedList<>();
     private LinkedList<Integer> maxMap = new LinkedList<>();
-    private int size = 0;
 
     public void push(int number){
         stackMap.addLast(number);
-        size++;
-        if (maxMap.isEmpty() || maxMap.getLast()<number)
+        if (maxMap.isEmpty() || maxMap.getLast()<=number)
             maxMap.addLast(number);
     }
 
@@ -19,7 +17,6 @@ public class MyStack {
         emptyException();
         int res = stackMap.getLast();
         stackMap.removeLast();
-        size--;
         if (maxMap.getLast()==res)
             maxMap.removeLast();
         return res;
