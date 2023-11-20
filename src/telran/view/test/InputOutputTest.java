2c8ed40c7c835cc14293c7d9e7a9fd362ce0f6de
yanfi-sher <loss.soul20@gmail.com>
2023-11-20 13:00:11 +0200

    with optional methods

--- src/telran/view/test/InputOutputTest.java
+++ src/telran/view/test/InputOutputTest.java
@@ -1,9 +1,10 @@
 package telran.view.test;
 
-import org.junit.Test;
-import telran.view.*;
 
-import static org.junit.Assert.*;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import telran.view.InputOutput;
+import telran.view.StandartInputOutput;
 
 record Point(int x, int y){
 
@@ -12,7 +13,8 @@ public class InputOutputTest {
     InputOutput io = new StandartInputOutput();
 
     @Test
-    public void readPointTest(){
+    @Disabled
+    void readPointTest(){
         Point point = io.readObject("enter coordinate of point; usage <x>#<y>",
                 "Wrong coordinates", str->{
                     String[] xy = str.split("#");
@@ -24,6 +26,19 @@ public class InputOutputTest {
                             return new Point(x,y);
                 }
         );
-        io.writeLine( point.x()+point.y()+"");
+        io.writeLine( point.x()+point.y());
+    }
+
+    @Test
+    void readObjectInteger(){
+        Integer number = io.readObject("Enter number in range [100-200]",
+                "Wrong number", str ->{
+            int num = Integer.parseInt(str);
+            if (num<100 || num>200){
+                throw new RuntimeException("Must be in range [100-200]");
+            }
+            return num;
+        });
+        io.writeLine(number/2);
     }
 }
